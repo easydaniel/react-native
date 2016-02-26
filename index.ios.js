22be1990b99deab116f11a1d395ca80a47b10b04
DanielChien <whitetiger50512@gmail.com>
2016-02-27 00:41:27 +0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} from 'react-native';
+
+class AwesomeProject extends Component {
+
+  constructor(props) {
+    super(props);
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2'])
+    };
+  }
+
+  render() {
+    return (
+      <View style={
+				styles.container
+			}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
